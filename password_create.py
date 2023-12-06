ebc153712ea8d071c3860906f3ef06ea31d9985d
compleXcore <103287377+compleXcore@users.noreply.github.com>
2023-12-06 18:03:56 +0300

    Update password_create.py

--- password_create.py
+++ password_create.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 password_repeat = int(input("Kaç adet şifre oluştursun(varsayılan 1): "))
 if password_repeat < 1:
@@ -8,17 +9,13 @@ password_lower = input("Şifrenizin hepsi küçük mü olsun?(E-H): ").upper()
 password_upper = input("Şifrenizin hepsi büyük mü olsun?(E-H): ").upper()
 if password_lower == 'E' and password_upper == 'E':
     print("Hem büyük hem küçük olsunu işaretlediniz. Sırayla bir büyük bir küçük şekilde şifreyi oluşturacak")
-password_specialchar = input("Hazır özel karakter eklenmesini istiyor musunuz?(!#$%&?@_)(E-H): ").upper()
-password_word = input("Eklenmesini istediğiniz birinci karakter veya sayıları girin: ")
-password_word2 = input("Eklenmesini istediğiniz ikinci karakter veya sayıları girin: ")
-password_word3 = input("Eklenmesini istediğiniz üçüncü karakter veya sayıları girin: ")
+password_word = string.ascii_letters + string.digits + string.punctuation
 
-def password_create(password_repeat, password_lenght, password_lower, password_upper, password_specialchar, password_word, password_word2, password_word3):
-    file = open(password_word+password_word2+password_word3+"_.txt", "a")
+def password_create(password_repeat, password_lenght, password_lower, password_upper, password_word):
+    file = open(str(password_repeat)+"_AdetSifre_.txt", "a")
     file.write(str(password_lenght)+" Uzunluğunda "+str(password_repeat)+" Adet Şifre oluşturdunuz\n")
     for i in range(password_repeat):
         newpassword=""
-        password_word = str(password_word)+str(password_word2)+str(password_word3) if password_specialchar == 'H' else str(password_word)+str(password_word2)+str(password_word3)+"!#$%&?@_"
         password = 0
         iRandom = 0
         while(password < password_lenght):
@@ -41,4 +38,4 @@ def password_create(password_repeat, password_lenght, password_lower, password_u
         file.write(newpassword+"\n")
     file.close()
 
-password_create(password_repeat=password_repeat, password_lenght=password_length, password_lower=password_lower, password_upper=password_upper, password_specialchar=password_specialchar, password_word=password_word, password_word2=password_word2, password_word3=password_word3)
+password_create(password_repeat=password_repeat, password_lenght=password_length, password_lower=password_lower, password_upper=password_upper, password_word=password_word)
